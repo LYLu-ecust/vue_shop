330a78b59d3d4b1d81df67a889e6271c77a1e11c
LYLu-ecust <1073171590@qq.com>
2020-03-18 09:08:16 +0800

    完成了登录功能

---
+++ src/components/login.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avater_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+         <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+          </el-form-item>
+          <el-form-item class="btns">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm:{
+        username:"admin",
+        password:"123456"
+      },
+      loginFormRules:{
+        // 验证用户名是否正确
+        username:[
+          {required:true,message:"请输入登录名称",triggle:"blur"},
+          {min:3,max:10,message:"长度在3~10个字符之内",triggle:"blur"}
+        ],
+        // 验证密码是否正确
+        password:[
+          {required:true,message:"请输入密码",triggle:"blur"},
+          {min:6,max:15,message:"长度在6~15个字符之内",triggle:"blur"}
+
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm(){
+      this.$refs.loginFormRef.resetFields();
+    },
+    login(){
+      this.$refs.loginFormRef.validate(async valid=>{
+         if(!valid) return;
+        const {data:res} = await this.$http.post('login',this.loginForm);
+        console.log(res);
+        if(res.meta.status !== 200){
+          return this.$message.error("登陆失败")
+        }
+        this.$message.success("登陆成功")
+        window.sessionStorage.setItem("token",res.data.token);
+        this.$router.push('/home')
+      })
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+  .login_container{
+    background-color: #2b4b6b;
+    height:100%;
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
+  }
+  .login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+
+    // 当使用：top: 50%;left: 50%;， 是以左上角为原点，故不处于中心位置
+    //translate(-50%,-50%) 作用是，往上（x轴）,左（y轴）移动自身长宽的 50%，以使其居于中心位置
+    position: absolute;
+    left: 50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+    .avater_box{
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left:50%;
+      transform:translate(-50%,-50%);
+      background-color: #fff;
+      img{
+        width:100%;
+        height:100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }  
+    }
+     .login_form{
+        position: absolute;
+        bottom: 0;
+        padding: 0 20px;
+        width: 100%;
+        box-sizing: border-box;
+      }
+      .btns{
+        display: flex;
+        justify-content: flex-end;
+      }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// 导入全局样式表
+import '../src/assets/css/global.css'
 Vue.config.productionTip = false
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
