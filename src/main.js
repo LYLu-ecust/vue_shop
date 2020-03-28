85dde50ad9203e542f93aa25e0ca4d058bbb2348
LYLu-ecust <1073171590@qq.com>
2020-03-28 20:37:00 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import '../src/assets/css/global.css'
 Vue.config.productionTip = false
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本剪辑器样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 // 配置axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -18,7 +24,18 @@ axios.interceptors.request.use(config =>{
 Vue.prototype.$http = axios
 import TreeTable from 'vue-table-with-tree-grid'
 Vue.component('tree-table',TreeTable)
-
+// 全局注册富文本编辑器
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat',function(originVal){
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = ((dt.getMonth()+1)+'').padStart(2,'0')
+  const d = (dt.getDate()+'').padStart(2,'0')
+  const hh = (dt.getHours()+'').padStart(2,'0')
+  const mm = (dt.getMinutes()+'').padStart(2,'0')
+  const ss = (dt.getSeconds()+'').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
